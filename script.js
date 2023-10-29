f28e6da46467280a77f682e3f7bb5f693cf4f48c
Robson <robsonribeirorra@gmail.com>
2023-10-29 10:22:56 -0300

    Adding filters

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const button = document.querySelector('.add-button');
 let tasksList = document.querySelector('.tasks-list');
 const emptyListDefault = document.querySelector('.empty-list');
 const deleteAll = document.querySelector('.delete-all');
-
+const utils = document.querySelector('.utils');
 
 tasksList.innerHTML = localStorage.getItem('tasks');
+
+let filter = 0;
 
 emptyListController();
 
@@ -18,38 +20,106 @@ input.addEventListener('keypress', (e) => {
     if(e.key === 'Enter') {
         addTask();
         saveTasks();
+        removeMsg();
     }
 });
 
 tasksList.addEventListener("click", (e) => {
     if(e.target.tagName === "BUTTON") {
         e.target.parentElement.remove();
-        saveTasks();
         emptyListController();
     } else if (e.target.tagName === "P") {
+        e.target.parentElement.classList.toggle('checked');
         let checkbox = e.target.previousElementSibling;
         if(checkbox.checked) {
             checkbox.checked = false;
-        } else {
-            checkbox.checked = true;
         }
     } else if (e.target.tagName === "LI") {
+        e.target.classList.toggle('checked');
         let checkbox = e.target.querySelector('input');
         if(checkbox.checked) {
             checkbox.checked = false;
-        } else {
-            checkbox.checked = true;
         }
+    } else if (e.target.tagName === 'INPUT') {
+        e.target.parentElement.classList.toggle('checked');
     }
+    saveTasks();
 });
 
-deleteAll.addEventListener("click", () => {
-    tasksList.innerHTML = '';
-    saveTasks();
+utils.addEventListener("click", (e) => {
+
+    let tasks;
+
+    if(e.target.classList[0] === "complete") {
+        if(filter !== 1) {
+            filter = 1;
+        } else {
+            tasksList.innerHTML = localStorage.getItem('tasks');
+            filter = 0;
+            return;
+        }
+        removeMsg();
+        tasksList.innerHTML = localStorage.getItem('tasks');
+        tasks = tasksList.querySelectorAll('.checked');
+        if(tasks.length < 1) {
+            let msg = document.createElement('div');
+            msg.classList.add('noTasks');
+            msg.innerText = "There's no completed task!";
+            tasksList.innerHTML = msg.outerHTML;
+            emptyListController();
+            return;
+        }
+        tasksList.innerHTML = '';
+        tasks.forEach(function (task) {
+            if(task.classList.value === "checked") {
+                tasksList.innerHTML += task.outerHTML;
+            }
+        });
+        emptyListController();
+    } else if(e.target.classList[0] === "incomplete") {
+        if(filter !== 2) {
+            filter = 2;
+        } else {
+            tasksList.innerHTML = localStorage.getItem('tasks');
+            filter = 0;
+            return;
+        }
+        removeMsg();
+        tasksList.innerHTML = localStorage.getItem('tasks');
+        tasks = tasksList.querySelectorAll('li');
+        let incompleteTasks = 0;
+        tasks.forEach( (task) => {
+            if(task.classList.value === '') incompleteTasks += 1;
+        })
+        if(incompleteTasks < 1) {
+            let msg = document.createElement('div');
+            msg.classList.add('noTasks');
+            msg.innerText = "There's no incomplete task!";
+            tasksList.innerHTML = msg.outerHTML;
+            emptyListController();
+            return;
+        }
+        tasksList.innerHTML = '';
+        tasks.forEach((task) => {
+            if(task.classList.value === '') {
+                tasksList.innerHTML += task.outerHTML; 
+            }
+        });
+        emptyListController();
+    } else if(e.target.classList.value === "delete-all") {
+        tasksList.innerHTML = '';
+        saveTasks();
+    }
+
     emptyListController();
 });
 
-
+const removeMsg = () => {
+    let msg = document.querySelector('.noTasks');
+    if(msg) {
+        msg.remove();
+    }
+}
 
 const addTask = () => {
     if(input.value) {
@@ -78,7 +148,7 @@ const addTask = () => {
 }
 
 const saveTasks = () => {
-    let tasks = document.querySelectorAll('li');
+    let tasks = tasksList.querySelectorAll('li');
     let tasksHTML = document.createElement('div');
     tasks.forEach(function (task) {
         tasksHTML.innerHTML += task.outerHTML;
